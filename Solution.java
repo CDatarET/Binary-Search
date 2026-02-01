1f638bf416ccdca9fec7408134dcf1d63cdd1878
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-02-01 22:58:23 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int helper(int[] nums, int target, int low, int high){
+        if(low > high){
+            return(-1);
+        }
+        if(target == nums[(low + high) / 2]){
+            return((low + high) / 2);
+        }
+        else if(target < nums[(low + high) / 2]){
+            return(helper(nums, target, low, (low + high) / 2 - 1));
+        }
+        else{
+            return(helper(nums, target, (low + high) / 2 + 1, high));
+        }
+    }
+
+    public int search(int[] nums, int target) {
+        return(helper(nums, target, 0, nums.length - 1));
+    }
+}
